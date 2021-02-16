a0bc9a7d62365d923610501bb1b5a7d1bebe0be3
Fernando Junior <fernando.junior85@outlook.com>
2021-02-16 19:31:58 -0300

    Adding bosons/Container

---
+++ src/components/bosons/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Container'
+})
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 85%;
+  max-width: 1150px;
+  margin: 0 auto;
+}
+</style>
